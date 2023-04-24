e7054f542969240cc06da1257779ddc66023e6c3
Sean Mei <42566561+mei-sean@users.noreply.github.com>
2023-04-24 11:57:59 -0700

    added css bootstrap

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 
+
 class UserRegistrationForm(forms.Form):
-    first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
-    username = forms.CharField(max_length=255)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First Name', 'style': 'width: 300px;', 'class': 'form-control'}),
+                                 max_length = 30, required=True)
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'style': 'width: 300px;', 'class': 'form-control'}),
+                                 max_length = 30, required=True)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder' :'Email', 'style': 'width: 300px;', 'class': 'form-control'}),
+                             max_length =30, required=True)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder' :'Password', 'style': 'width: 300px;', 'class': 'form-control'}),
+                             max_length =30, required=True)
     # add any other fields you want to include in the registration form
